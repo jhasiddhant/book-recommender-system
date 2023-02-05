371841fec3d001f87a2f8d3aa006988a21e06052
Siddhant Jha <jhasiddhant@hotmail.com>
2023-02-05 22:52:02 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# Book Recommendation System
+# Collaborative Filtering Based Book Recommender System
+
+## Introduction
+This is a collaborative book recommender system that suggests similar books based on user's reading history and other users' preferences.
+
+## Requirements
+To run this recommendation system, you will need the following:
+
+Python 3.9.12
+Pandas
+Numpy
+Scikit-learn 
+
+## Dataset
+The system uses 3 datasets: books, users and ratings. The datasets can be downloaded from the given link: https://www.kaggle.com/datasets/saurabhbagchi/books-dataset
+
+## Implementation
+
+The recommendation system is implemented in the brs.py file. The code performs the following steps:
+
+1. Cleaning dataset
+
+2. Selecting users and books with more than 100 books.
+
+3. Applying Pivot Table.
+
+4. Calculate the cosine similarity.
+
+5. Make recoomendations of similar books to the users based on their reading history
+
+## Installation
+
+Clone this repository to your local machine:
+
+```bash
+git clone https://github.com/jhasiddhant/book-recommender-system.git
+```
+Install the required packages:
+```bash
+pip install -r requirements.txt
+```
+Run the brs.py file:
+```bash
+python brs.py
+```
+When prompted, enter the name of a book to receive recommendations for book based on users history who have read that book.
+
+## Example:
+Enter book name: a painted house
+
+Recommended books :
+<br />
+The Brethren
+<br />
+We Were The Mulvaneys
+<br />
+The Firm
+<br />
+The Pelican Brief
+<br />
+Cradle And All
+<br />
+The Chamber
+<br />
+The Testament
+<br />
+The Lovely Bones: A Novel
+<br />
+The King Of Torts
+<br />
+Fall On Your Knees
--- brs.py
+++ brs.py
@@ -4,29 +4,20 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Loading datasets
-books = pd.read_csv("./dataset/books.csv", sep=";",
-                    error_bad_lines=False, encoding="latin-1")
-ratings = pd.read_csv("./dataset/ratings.csv", sep=";",
-                      error_bad_lines=False, encoding="latin-1")
-users = pd.read_csv("./dataset/users.csv", sep=";",
-                    error_bad_lines=False, encoding="latin-1")
+books = pd.read_csv("./dataset/books.csv", sep=";", error_bad_lines=False, encoding="latin-1")
+ratings = pd.read_csv("./dataset/ratings.csv", sep=";", error_bad_lines=False, encoding="latin-1")
+users = pd.read_csv("./dataset/users.csv", sep=";", error_bad_lines=False, encoding="latin-1")
 
 books.columns = ['ISBN', 'Title', 'Author', 'Year', 'Publisher', 'imageUrlS', 'imageUrlM', 'imageUrlL']
 users.columns = ['userID', 'Location', 'Age']
 ratings.columns = ['userID', 'ISBN', 'Rating']
 
-# Data Cleaning
 
-# Changing book titles to titlecase
+# Cleaning and merging datasets
+books.columns = ['ISBN', 'Title', 'Author', 'Year', 'Publisher', 'imageUrlS', 'imageUrlM', 'imageUrlL']
 books['Title'] = books['Title'].apply(str.title)
-
-# dropping all the null values
 books.dropna(inplace=True)
-
-# drop image links
 books.drop(['imageUrlS', 'imageUrlM', 'imageUrlL'],axis=1,inplace=True)
-
-# Merge ratings dataset with book datset
 book_df = ratings.merge(books, on='ISBN')
 
 
@@ -35,13 +26,11 @@ book_df = ratings.merge(books, on='ISBN')
 # selecting users who have rated more than 100 books
 rating_users = book_df.groupby('userID').count()['Rating'] > 100
 rating_users = rating_users[rating_users].index
-
 filtered_rating = book_df[book_df['userID'].isin(rating_users)]
 
 # selecting books with more than 50 ratings
 famous_books = filtered_rating.groupby('Title').count()['Rating'] >= 100
 famous_books = famous_books[famous_books].index
-
 final_rating = filtered_rating[filtered_rating['Title'].isin(famous_books)]
 
 # Drop duplicates
@@ -54,7 +43,7 @@ matrix = final_rating.pivot_table(index='Title', columns='userID', values='Ratin
 similarity_score = cosine_similarity(matrix)
 
 
-def recommend_me_books(book_name):
+def recomendations(book_name):
     # getting the index of the book_name
     index_of_book = np.where(matrix.index == book_name)[0][0]
     
@@ -62,13 +51,19 @@ def recommend_me_books(book_name):
     similar_books_index = sorted(list(enumerate(similarity_score[index_of_book])), key= lambda x:x[1], reverse = True)[1:11]
     
     # printing the names of the similar books
-    print(f'You Like : \t{book_name}.')
-    print('\nYou may also like these : ')
+    print('\nRecommended books : ')
     for item in similar_books_index:
         book_index = item[0]
         same_type_book = matrix.index[book_index]
         print(f'{same_type_book}')
 
+# recommend_me_books('A Painted House')
 
-recommend_me_books('A Painted House')
+# Taking user input for movie name
+book_name = input("Enter book name: ")
 
+# Converting movie name to uppercase
+x = book_name.title()
+
+# Calling the recommendation function
+recomendations(x)
